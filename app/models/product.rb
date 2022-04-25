c9dfc5de63b8e2f123d7d635c4980a7428be82e1
haaaanaaa <hanahiraga@gmail.com>
2022-04-25 18:38:52 +0900

    商品出品機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -30,8 +30,10 @@ class Product < ApplicationRecord
     validates :status_id
   end
 
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'Price Half-width number' },inclusion: { in: (300..9_999_999), message: 'Out of setting range' }
   
   
-                
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: 'Half-width number' }
+  validates :price,
+            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
+  
 end
