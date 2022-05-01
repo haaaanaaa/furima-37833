42c778edba349afa2de42c243fc50c4def73584b
haaaanaaa <hanahiraga@gmail.com>
2022-05-01 16:58:29 +0900

    消去機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :require_login, only: :new, alert: 'You need to sign in or sign up before continuing.'
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -41,7 +41,13 @@ class ProductsController < ApplicationController
     end
   end
 
-  
+  def destroy
+    if @product.destroy
+      redirect_to root_path
+    else
+      redirect :show
+    end
+  end
 
   private
 
